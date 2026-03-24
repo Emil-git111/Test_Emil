9a25d3e3ef5e0d4affcebcee04f5dee963f41fe0
Emil Seibitz <emil.seibitz@bulme.at>
2026-03-24 13:40:00 +0100

    Das Pogramm kann crcumference berechnen. Die Datedypen muessen noch angepasst werden. Das Projekt befindet sich im ordner Dreieck.

---
+++ Dreieck/src/Main.java
@@ -0,0 +1,10 @@
+void main(){
+  int a = 20;
+  int b = 10;
+  int c = 5;
+
+  circumference umfang = new circumference(a,b,c);
+
+  System.out.println("Der umfang ist: "+ umfang.umfang(a,b,c));
+
+}
---
+++ Dreieck/src/circumference.java
@@ -0,0 +1,19 @@
+public class circumference {
+    public int a;
+    public int b;
+    public int c;
+
+    public circumference(int a,int b,int c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public int umfang(int a,int b,int c)
+    {
+
+        return a + b + c;
+
+    }
+
+}
